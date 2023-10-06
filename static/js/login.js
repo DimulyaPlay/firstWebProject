dfeb877640ab8703d1bf96e40c8607a2ebedab6c
DimulyaPlayHome <dimasiksosnin@mail.ru>
2023-10-07 00:24:27 +0500

    setting static ips

--- static/js/login.js
+++ static/js/login.js
@@ -1,12 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const BASE_URL = 'https://188.64.15.189:8443';
     const loginForm = document.getElementById('loginForm');
     loginForm.addEventListener('submit', (e) => {
         e.preventDefault();
         const username = document.getElementById('username').value;
         const password = document.getElementById('password').value;
-        const url = `${BASE_URL}/loginUser`;
-        fetch(url, {
+        fetch('/loginUser', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
